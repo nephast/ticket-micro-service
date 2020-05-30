3fcac5ea9e88d39424a1e8d27cbcee37587aaa78
Stéphan Gaud <stephan.gaud@sainsburys.co.uk>
2020-05-30 20:52:36 +0100

    add formatted errors

--- auth/src/middewares/error-handler.ts
+++ auth/src/middewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection';
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,18 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
 
-  res.status(400).send({
-    message: err.message,
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+
+  res.status(500).send({
+    errors: [{ message: 'Something went wrong' }],
   });
 };
