7c3f885091b4e817761d8cf20b4eea1277b72ec1
Stéphan Gaud <stephan.gaud@sainsburys.co.uk>
2020-05-30 21:29:13 +0100

    implement abstract class for error handling

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,12 @@
+import { CustomError } from './custom-error';
 import { ValidationError } from 'express-validator';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
-    super();
+    //message only for logging purposes 
+    super('Invalid request parameters');
 
     //because we are extending a build in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
--- auth/src/middewares/error-handler.ts
+++ auth/src/middewares/error-handler.ts
@@ -1,7 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection';
-import { serialize } from 'v8';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -9,11 +7,7 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
