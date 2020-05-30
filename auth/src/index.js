d127e52b34faae4d9999e358d7fc5987f1007832
Stéphan Gaud <stephan.gaud@sainsburys.co.uk>
2020-05-30 19:54:54 +0100

    apply Prettier single quotes, add error middleware

--- auth/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var body_parser_1 = require("body-parser");
-var app = express_1["default"]();
-app.use(body_parser_1.json());
-app.listen(3000, function () {
-    console.log("listening on port 3000");
-});
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,29 +1,29 @@
-import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
 router.post(
-  "/api/users/signup",
+  '/api/users/signup',
   [
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
+      .withMessage('Password must be between 4 and 20 characters'),
   ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error('Imvalid email or password');
     }
 
     const { email, password } = req.body;
 
-    console.log("creating user");
+    console.log('creating user');
 
-    res.send({ data: "yo" });
+    res.send({ data: 'yo' });
   }
 );
 
