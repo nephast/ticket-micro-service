247c21fccc5dfed4f09073871c0889f31371f4e8
Stéphan Gaud <stephan.gaud@sainsburys.co.uk>
2020-06-06 19:35:08 +0100

    signin flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
+import { Password } from '../services/password';
+import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -15,8 +19,36 @@ router.post(
       .withMessage('You must supply a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send('hi there');
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordMatched = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatched) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    //store JWT on session obj
+    //TODO sort out type here
+    req.session!['jwt'] = userJwt;
+
+    res.status(200).send(existingUser);
   }
 );
 
