aaefb155861d1d60decc403ac2b24ced420bde95
Stéphan Gaud <stephan.gaud@sainsburys.co.uk>
2020-06-21 16:38:29 +0100

    add signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,27 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('return a 400 with invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('return a 400 with invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+it('return a 400 with invalid request', async () => {
+  return request(app).post('/api/users/signup').send({}).expect(400);
+});
