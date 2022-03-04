ab390d3882b784cb6261dc6891ea914a18ae28fc
nephast <stephan.gaud@sainsburys.co.uk>
2022-03-04 18:52:08 +0000

    getInitialProps for auth (server and client)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,30 @@
-export default () => {
-  return <h1>hello</h1>;
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on the server
+    // requests should be made to http://ingress-ngnix.ingress-ngnix...
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser
+    // request should be made with base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
